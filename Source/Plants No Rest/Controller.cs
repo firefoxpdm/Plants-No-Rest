8d998be386418f1930468a8c360509512a687f44
Tamas.Szekeres <firefoxpdm@yahoo.co.uk>
2020-03-13 22:27:09 +0100

    1.1 baseline

---
+++ Source/Plants No Rest/Controller.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace Plants_No_Rest
+{
+    public class Controller : Mod
+    {
+        public Controller(ModContentPack content) : base(content)
+        {
+            Harmony harmony = new Harmony("com.firefox.plantsnorest");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Source/Plants No Rest/PlantRestOverride.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+
+namespace Plants_No_Rest
+{
+    class PlantRestOverride
+    {
+
+        [HarmonyPatch(typeof(Plant))]
+        [HarmonyPatch("Resting", MethodType.Getter)]
+        static class CharacterCardUtility_DrawCharacterCard
+        {
+            public static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                return false;
+            }
+        }
+    }
+}
